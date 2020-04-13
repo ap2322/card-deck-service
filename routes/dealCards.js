e6194f1eebc06ebf699fcc761de5dd39fd04b04b
Alice Post <alicep2322@gmail.com>
2020-04-13 15:37:11 -0600

    Have dealt deck save in db on the dealCards POST route

--- routes/dealCards.js
+++ routes/dealCards.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const router = express.Router();
 // make model for card deck and import it
+const Deck = require('../models/deck.js')
+const DealtCards = require('../models/dealtCards.js')
+
+
+
+router.post("", (req, res, next) => {
+  console.log('made it to router')
+
+  const deck = new Deck();
+  const dealtCards = new DealtCards({
+    dealtCardMatrix: deck.shuffle().dealInMatrix()
+  })
+
+  dealtCards.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'Cards shuffled and dealt successfully',
+        data: result
+      })
+    })
+    .catch(result => console.log('Error saving a new post'))
+});
+
+module.exports = router;
