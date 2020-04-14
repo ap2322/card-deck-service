c8df3eaceb0d9b4e12d1914bf7303139d1a5642f
Alice Post <alicep2322@gmail.com>
2020-04-14 14:31:36 -0600

    Conditionally connect to either dev, test, or production mongodb instances

--- app.js
+++ app.js
@@ -8,15 +8,34 @@ const dealCardsRoutes = require("./routes/dealCards")
 
 const app = express();
 const environment = process.env.NODE_ENV || 'development';
-
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0-0bjmx.mongodb.net/${environment}?retryWrites=true&w=majority`,
+console.log("environment", environment)
+if (environment === 'production') {
+  mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@cluster0-0bjmx.mongodb.net/${environment}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to database!")
-  })
-  .catch(() => {
-    console.log("Connection failed")
-  })
+    .then(() => {
+      console.log("Connected to database!")
+    })
+    .catch(() => {
+      console.log("Connection failed")
+    })
+} else if (environment === 'development') {
+  mongoose.connect("mongodb://localhost/cardDeck_dev",
+    { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connection
+    .once('open', () => console.log('Connected to cardDeck_dev'))
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    })
+} else if (environment === 'test') {
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/cardDeck_test',
+      { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connection
+      .once('open', () => console.log('Connected to cardDeck_test'))
+      .on('error', (error) => {
+        console.warn('Warning', error);
+      })
+}
 
 app.use(bodyParser.json());
 
